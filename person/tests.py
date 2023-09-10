eef2f0ed0628d997053178fab97e18e0cfe83e0d
Brian Obot <brianobot9@gmail.com>
2023-09-10 17:44:13 +0100

    Update tests

--- person/tests.py
+++ person/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+import pytest
+import requests
+from django.urls import reverse
+from rest_framework.test import APIClient
+from rest_framework.response import Response
 
-# Create your tests here.
+from person.models import Person
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.mark.django_db
+def test_list_persons(client: APIClient):
+    url = reverse("persons:person-list")
+    response: Response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_create_person(client: APIClient):
+    url = reverse("persons:person-list")
+    data = {
+        'name': "Brian David Obot"
+    }
+    response: Response = client.post(url, data)
+    assert response.status_code == 201
+
+
+@pytest.mark.django_db
+def test_person_detail(client: APIClient):
+    Person.objects.create(name="James")
+    url = reverse("persons:person-detail", args=("James",))
+    response: Response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test_update_person(client: APIClient):
+    jame = Person.objects.create(name="James")
+    url = reverse("persons:person-detail", args=("James",))
+    response: Response = client.put(url, data={"name": "Jamie"})
+    jame.refresh_from_db()
+    assert response.status_code == 200
+    assert jame.name == "Jamie"
+
+
+@pytest.mark.django_db
+def test_delete_person(client: APIClient):
+    Person.objects.create(name="James")
+    url = reverse("persons:person-detail", args=("James",))
+    response: Response = client.delete(url)
+    assert response.status_code == 204
