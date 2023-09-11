08ee7ad507a9e5e99a6ef0dace0be505f54e61c5
Brian Obot <brianobot9@gmail.com>
2023-09-11 10:14:10 +0100

    Remove pagination for later

--- core/settings.py
+++ core/settings.py
@@ -117,8 +117,8 @@ REST_FRAMEWORK = {
         # "rest_framework.permissions.IsAuthenticated",
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 20
 }
 
 
